4fa2f919928866529cdcd854c2b9f5c77d0f1494
fedebarranco <fbarranco97@gmail.com>
2023-04-03 21:04:17 -0300

    Primer Commit

---
+++ src/HolaMundo.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class HolaMundo {
+
+    public static void main(String args[]) {
+
+        inicio:
+        for (var contador = 0; contador < 3; contador++) {
+            if (contador % 2 != 0) {
+                continue inicio;
+            }
+            System.out.println("contador = " + contador);
+        }
+
+//        var contador = 0;
+//        while (contador < 3) {
+//            System.out.println("contador = " + contador);
+//            contador++;
+//
+//        }
+//        var contador = 0;
+//        do {
+//            System.out.println("contador = " + contador);
+//            contador++;
+//        } while (contador < 3);
+    }
+}
